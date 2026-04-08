cf956de8881f84cf9f13ccdc2cf9abc81651f0f8
Ana <anaclimgo@gmail.com>
2026-04-08 14:52:20 -0300

    feat: created po agent service

---
+++ backend/app/services/agents/po_agent.rb
@@ -0,0 +1,12 @@
+class Agents::PoAgent
+  def initialize(project)
+    @project = project
+  end
+
+  def call(input)
+    # chama API da Stratus
+    # transforma resposta em tasks
+    # salva tasks
+    # cria logs
+  end
+end
