a604b2633685662e543177e0f5299808ea84a881
Ana <anaclimgo@gmail.com>
2026-04-08 15:00:55 -0300

    feat: re-created project model and migrated to db

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_08_175525) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_08_180023) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "status"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,4 +30,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_08_175525) do
     t.string "password_digest"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "projects", "users"
 end
