8bd655840e624d0c3bc155c18ebdc9348f954904
Ana <anaclimgo@gmail.com>
2026-04-12 10:55:21 -0300

    feat: migrated to schema description column on projects table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_12_134903) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_12_135413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_12_134903) do
 
   create_table "projects", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.text "description"
     t.string "name"
     t.string "status"
     t.datetime "updated_at", null: false
