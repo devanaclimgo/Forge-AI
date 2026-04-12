75b68f36f5ea4b1b9e2ef257b9350dd5a8cac4ef
Ana <anaclimgo@gmail.com>
2026-04-12 10:49:57 -0300

    feat: migrated application record

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_12_134345) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_12_134903) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -23,6 +23,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_12_134345) do
     t.bigint "project_id", null: false
     t.datetime "updated_at", null: false
     t.index ["project_id"], name: "index_agent_logs_on_project_id"
+  end
+
+  create_table "application_records", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "cost_records", force: :cascade do |t|
