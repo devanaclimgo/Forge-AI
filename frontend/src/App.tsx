76260a49f360bbd3d0cc2c2bb0b3a528b286ac07
Ana <anaclimgo@gmail.com>
2026-04-17 18:20:34 -0300

    fix: update App component to use Routes and add BrainDumpPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
-import AgentsPage from "./pages/AgentPage"
+import { Routes, Route } from "react-router-dom";
+import AgentPage from "./pages/AgentPage";
+import BrainDumpPage from "./pages/BrainDumpPage";
 
 export default function App() {
-  return <AgentsPage />
+  return (
+    <Routes>
+      <Route path="/" element={<AgentPage />} />
+      <Route path="/brain-dump" element={<BrainDumpPage />} />
+    </Routes>
+  );
 }
