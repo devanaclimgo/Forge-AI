d040cf5f6906731282be81a05623cef11dbf4e6b
Ana <anaclimgo@gmail.com>
2026-04-20 18:20:58 -0300

    refactor: streamline task controller methods and enhance readability

--- backend/app/controllers/api/v1/tasks_controller.rb
+++ backend/app/controllers/api/v1/tasks_controller.rb
@@ -1,13 +1,50 @@
 class Api::V1::TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  def index
+    tasks = current_user.projects
+      .find(params[:project_id])
+      .tasks
+      .includes(:sprint)
+    render json: tasks
+  end
+
+  def show
+    render json: @task
+  end
+
+  def create
+    task = current_project.tasks.create!(task_params)
+    render json: task, status: :created
+  end
+
   def update
-    task = Task.find(params[:id])
+    previous_status = @task.status
+    @task.update!(task_params)
 
-    task.update!(task_params)
-
-    if task.status == "done"
-      AgentOrchestrator.task_completed(task)
+    if previous_status != "done" && @task.status == "done"
+      AgentOrchestrator.task_completed(@task)
     end
 
-    render json: task
+    render json: @task
+  end
+
+  def destroy
+    @task.destroy!
+    head :no_content
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def current_project
+    current_user.projects.find(params[:project_id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :status, :priority, :category, :sprint_id)
   end
 end
