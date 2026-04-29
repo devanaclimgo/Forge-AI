0e996d6fd6d77515202b7bdab99c715c84d78188
Ana <anaclimgo@gmail.com>
2026-04-29 16:22:46 -0300

    feat: add dependent destroy option to associations in Project model

--- backend/app/models/project.rb
+++ backend/app/models/project.rb
@@ -1,8 +1,7 @@
 class Project < ApplicationRecord
   belongs_to :user
-
-  has_many :tasks
-  has_many :sprints
-  has_one :project_context
-  has_many :agent_logs
+  has_many :tasks, dependent: :destroy
+  has_many :sprints, dependent: :destroy
+  has_one  :project_context, dependent: :destroy
+  has_many :agent_logs, dependent: :destroy
 end
